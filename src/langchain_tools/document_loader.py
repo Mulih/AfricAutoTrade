9bab37538a3c1470a39e84114ffbf43ba0b7537d
Mulih <almulih@yahoo.com>
2025-05-12 13:08:45 +0300

    Add document loader function to load text documents from a specified folder

---
+++ src/langchain_tools/document_loader.py
@@ -0,0 +1,15 @@
+from langchain.document_loaders import TextLoader
+
+def load_documents_from_folder(folder_path: str):
+    """
+    Load all text documents from a specified folder.
+
+    Args:
+        folder_path (str): The path to the folder containing text documents.
+
+    Returns:
+        list: A list of loaded documents.
+    """
+    loader = TextLoader(folder_path)
+    documents = loader.load()
+    return documents
