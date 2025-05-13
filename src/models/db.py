005c8032ee101bf71d89181c9ca8ce91797c0dae
Mulih <almulih@yahoo.com>
2025-05-13 14:02:21 +0300

    remove debug print statement for database URL in db.py

--- src/models/db.py
+++ src/models/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from src.settings.config import settings
 
-print("-> Database URL:", settings.database_url)
+
 # Create a new SQLAlchemy engine instance
 
 engine = create_engine(settings.database_url, echo=True, future=True)
